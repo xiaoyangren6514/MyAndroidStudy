524ea7842b205820c3c3ae6802a5594e8e1cd617
zhonglongquan <zhonglongquan@rong360.com>
2016-09-22 13:53:10 +0800

    修改色值，微调

--- customer_view/src/main/java/com/happy/xyr/view/HookView.java
+++ customer_view/src/main/java/com/happy/xyr/view/HookView.java
@@ -16,16 +16,12 @@ import com.happy.xyr.utils.Utils;
 
 public class HookView extends View {
 
-    //绘制圆弧的进度值
     private int progress = 0;
-    //线1的x轴
-    private int line1_x = 0;
-    //线1的y轴
-    private int line1_y = 0;
-    //线2的x轴
-    private int line2_x = 0;
-    //线2的y轴
-    private int line2_y = 0;
+
+    private float line1_x = 0;
+    private float line1_y = 0;
+    private float line2_x = 0;
+    private float line2_y = 0;
 
     private int min_size;
 
@@ -47,7 +43,7 @@ public class HookView extends View {
     private void init() {
         min_size = (int) Utils.dp2px(getResources(), 67);
         paint = new Paint();
-        paint.setColor(getResources().getColor(R.color.arc_blue));
+        paint.setColor(getResources().getColor(R.color.color_blue));
         paint.setStrokeWidth(8);
         paint.setStyle(Paint.Style.STROKE);
         paint.setAntiAlias(true);
@@ -85,48 +81,37 @@ public class HookView extends View {
          * 绘制圆弧
          */
 
-        //获取圆心的x坐标
         int center = getWidth() / 2;
         int center1 = center - getWidth() / 4;
-        //圆弧半径
         int radius = getWidth() / 2 - 5;
 
-        //定义的圆弧的形状和大小的界限
         RectF rectF = new RectF(center - radius - 1, center - radius - 1, center + radius + 1, center + radius + 1);
         canvas.drawArc(rectF, 0, 360 * progress / 100, false, paint);
 
         /**
          * 绘制对勾
          */
-        //先等圆弧画完，才话对勾
         if (progress >= 100) {
-            if (line1_x < radius / 3) {
-                line1_x++;
-                line1_y++;
+            if (line1_x <= radius / 3) {
+                line1_x = line1_x + 2;
+                line1_y = line1_y + 2;
             }
-            //画第一根线
             canvas.drawLine(center1, center, center1 + line1_x, center + line1_y, paint);
 
-            if (line1_x == radius / 3) {
+            if (line1_x <= radius / 3) {
                 line2_x = line1_x;
                 line2_y = line1_y;
-                line1_x++;
-                line1_y++;
+                line1_x = line1_x + 2;
+                line1_y = line1_y + 2;
             }
             if (line1_x >= radius / 3 && line2_x <= radius) {
-                line2_x++;
-                line2_y--;
+                line2_x = line2_x + 2;
+                line2_y = line2_y - 2;
             }
-            //画第二根线
-            canvas.drawLine(center1 + line1_x - 1, center + line1_y, center1 + line2_x, center + line2_y, paint);
+            canvas.drawLine(center1 + line1_x - 2, center + line1_y, center1 + line2_x, center + line2_y, paint);
         }
 
-        //每隔3毫秒界面刷新
-        if (progress < 100) {
-            postInvalidateDelayed(2);
-        }else {
-            postInvalidate();
-        }
+        postInvalidate();
     }
 
 }
